3f026dca2ac68254fbb5275b627319b73172b34e
vittoriopennella94 <54901894+vittoriopennella94@users.noreply.github.com>
2025-11-04 07:39:34 +0100

    init commit

---
+++ awesome-pizza/src/main/java/it/adesso/awesomepizza/AwesomePizzaApplication.java
@@ -0,0 +1,13 @@
+package it.adesso.awesomepizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwesomePizzaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwesomePizzaApplication.class, args);
+    }
+
+}
---
+++ awesome-pizza/src/test/java/it/adesso/awesomepizza/AwesomePizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.adesso.awesomepizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwesomePizzaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
