8381766657ec32e42d3d71399106d479cb067c61
Vittorio Pennella <v.pennella94@gmail.com>
2025-11-16 10:23:25 +0100

    aggiunta la java doc per la classe utils

--- awesome-pizza/src/main/java/it/adesso/awesomepizza/utility/Utils.java
+++ awesome-pizza/src/main/java/it/adesso/awesomepizza/utility/Utils.java
@@ -2,9 +2,30 @@ package it.adesso.awesomepizza.utility;
 
 import it.adesso.awesomepizza.enums.OrderStateEnum;
 
-import java.text.MessageFormat;
+import java.util.IllegalFormatException;
 
+
+/**
+ * Utility class
+ */
 public class Utils {
+
+
+    /**
+     * Static function to check for correct state change
+     *
+     * <p>The possible state transition are</p>
+     * <ol>
+     *     <li>From IN_ATTESA to IN_PREPARAZIONE or ANNULLATO</li>
+     *     <li>From IN_PREPARAZIONE to IN_CONSEGNA or ANNULLATO</li>
+     *     <li>FROM IN_CONSEGNA to CONSEGNATO</li>
+     * </ol>
+     *
+     *
+     * @param stateIdFrom Current order status
+     * @param stateIdTo New order status
+     * @return true if the state transition is possible, false if the state transition is NOT possible
+     */
     public static boolean checkIfChangeStateOk(Long stateIdFrom, Long stateIdTo) {
         boolean result = false;
 
@@ -23,6 +44,15 @@ public class Utils {
         return  result;
     }
 
+    /**
+     * Formats a message string with the provided arguments.
+     *
+     * @param message the message template with format specifiers (e.g., %s, %d)
+     * @param args the arguments to replace the placeholders in the message
+     * @return the formatted message string
+     * @throws IllegalFormatException if the format string is invalid or the arguments don't match
+     * @see String#format(String, Object...)
+     */
     public static String formatMessage(String message, Object ... args) {
         return String.format(message, args);
     }
